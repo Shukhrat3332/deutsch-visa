519d26f56ecd1b261f46cab5af6b88975c9ed617
Shukhrat3332 <shuhratavezov@gmail.com>
2026-08-15 08:49:46 +0200

    Create session.ts

---
+++ src/lib/session.ts
@@ -0,0 +1,10 @@
+export function getSessionId(): string {
+  const key = "dv_session_id";
+  let id = window.localStorage.getItem(key);
+  if (!id) {
+    id = crypto.randomUUID();
+    window.localStorage.setItem(key, id);
+  }
+  return id;
+}
+
